d3aec4bada35e2be7a31641d9ace2f6f82c1347f
Daimon-27 <skabaradmytro27@gmail.com>
2025-01-28 14:53:03 +0200

    calc start

--- Calc.py
+++ Calc.py
@@ -1,2 +1,15 @@
-print('hello world')
-print('hi')
+num1 = float(input('Введіть перше число:'))
+num2 = float(input('Введіть друге число:'))
+operation = input('Ведіть арифметичну дію:')
+
+def get_sum(a, b):
+    print(a + b)
+
+def get_dffrnc(a, b):
+    print(a - b)
+
+def get_dffrnc(a, b):
+    print(a - b)
+
+get_sum(num1,num2)
+get_dffrnc(num1,num2)
